fd570614913d8609fa000e238ae257a7de6af246
woodley <gelinw3@gmail.com>
2023-07-03 17:36:37 -0400

    add route to add a campus to database

--- api/campuses.js
+++ api/campuses.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const {campuses} = require("../db/models");
 
+//use json middleware to notiy server that im taking json middleware
+router.use(express.json());
 router.get("/", async(req, res, next) =>{
     try {
         const allCampuses = await campuses.findAll();
@@ -11,5 +13,14 @@ router.get("/", async(req, res, next) =>{
         next(error);
     }
 });
-
+router.post("/AddCampus", async(req, res, next) => {
+    try {
+        const data = req.body;
+        console.log("printing data", data);
+        await campuses.create(data);
+        res.status(201).json({ message: 'Post created successfully' });
+    } catch (error) {
+        next(error);
+    }
+});
 module.exports = router;
