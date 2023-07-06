f8fd3154e1ac93c8810212a0f328368cc79ed205
woodley <gelinw3@gmail.com>
2023-07-06 12:35:23 -0400

    change campuseNames and campusOne imageUrl

--- seed.js
+++ seed.js
@@ -3,25 +3,25 @@ const {campuses, students} = require("./db/models");
 
 const CampusesSeed = [
     {
-        name: 'Campus 1',
-        imageUrl: 'https://i.ibb.co/9yCwGK7/images.jpg',
+        name: 'CampusOne',
+        imageUrl: 'https://i.ibb.co/zV3jKZT/Brooklyn-College-Library-1200x628.jpg',
         address: '123 Main St, City 1',
         description: 'Description for Campus 1'
       },
       {
-        name: 'Campus 2',
+        name: 'CampusTwo',
         imageUrl: 'https://i.ibb.co/kyy6R3Q/istockphoto-1323420737-612x612.jpg',
         address: '456 Elm St, City 2',
         description: 'Description for Campus 2'
       },
       {
-        name: 'Campus 3',
+        name: 'CampusThree',
         imageUrl: 'https://i.ibb.co/LZMdcJZ/photo-1562774053-701939374585.jpg',
         address: '789 Oak St, City 3',
         description: 'Description for Campus 3'
       },
       {
-        name: 'Campus 4',
+        name: 'CampusFour',
         imageUrl: 'https://i.ibb.co/MpWn8c8/photo-1607237138185-eedd9c632b0b.jpg',
         address: '987 Pine St, City 4',
         description: 'Description for Campus 4'
